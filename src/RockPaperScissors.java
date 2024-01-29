71c90a62a229830f0523c7f7acd2b269e639034f
Seikou <104414846+selly361@users.noreply.github.com>
2024-01-29 15:38:51 +0000

    Update: add scores

--- src/RockPaperScissors.java
+++ src/RockPaperScissors.java
@@ -6,6 +6,9 @@ public class RockPaperScissors {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int computer_score = 0;
+        int user_score = 0;
+
         while (true) {
             System.out.println("Enter your choice (Rock, Paper, Scissors) or 'exit' to end: ");
             String userChoice = scanner.nextLine().toLowerCase();
@@ -13,7 +16,9 @@ public class RockPaperScissors {
 
             if (userChoice.equals("exit")) {
                 System.out.println("Thanks for playing. Goodbye!");
-                break;  
+                user_score = 0;
+                computer_score = 0;
+                break;
             }
 
             else if(!Set.of("rock", "paper", "scissors").contains(userChoice)){
@@ -28,12 +33,13 @@ public class RockPaperScissors {
 
             userChoice = userChoice.substring(0, 1).toUpperCase() + userChoice.substring(1);
 
-
-            System.out.println(String.format("You Picked %s | Computer picked %s", userChoice, computerChoice));
-
-
             RockPaperScissorsGame game = new RockPaperScissorsGame(userChoice.toLowerCase(), computerChoice.toLowerCase());
-            game.play();
+            int[] results = game.play(user_score, computer_score);
+
+            user_score = results[0];
+            computer_score = results[1];
+
+            System.out.println(String.format("You Picked %s (Score: %d) | Computer picked %s (Score: %d)", userChoice, user_score, computerChoice, computer_score));
         }
 
         scanner.close();
@@ -49,14 +55,20 @@ class RockPaperScissorsGame {
         this.computerChoice = computerChoice;
     }
 
-    public void play() {
+    public int[] play(int user_score, int computer_score) {
         if (userChoice.equals(computerChoice)) {
             System.out.println("It's a tie!");
         } else if (isUserWinning()) {
+            user_score+=1;
             System.out.println("Congratulations! You win!");
         } else {
             System.out.println("Computer wins. Better luck next time!");
+            computer_score+=1;
         }
+
+        int [] scores = {user_score, computer_score};
+
+        return scores;
     }
 
     private boolean isUserWinning() {
